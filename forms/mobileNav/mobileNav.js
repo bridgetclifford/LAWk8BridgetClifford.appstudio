ff2b257f104cb379b671ef61fac69ccacd060c09
bridgetclifford <90580691+bridgetclifford@users.noreply.github.com>
2021-10-27 15:43:48 -0500

    mobileNav

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -1,7 +1,28 @@
 
+mobileNav.onshow=function(){
+  hmbMenu.hidden = false
+}
+
+
 hmbMenu.onclick=function(s){
  // add code to check if user clicked the hamburger control to open it 
-  /* add a switch here to take the user to the form
-     they chose. 
-  */
+   if (typeof(s) == "object") { // do nothing - they just clicked on the control
+       return
+    } else {
+       switch(s) {
+            case "Dessert Voting":
+                ChangeForm(dessertVoting)
+                break
+            case "Describe You":
+                ChangeForm(describeYou)
+                break
+            case "Favorite Exercises":
+                ChangeForm(favExercises)
+                break
+            case "Mobile Nav":
+                ChangeForm(mobileNav)
+                break
+       }  
+   } 
 }
+
